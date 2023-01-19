56dd08f088d2d21b008cf5998a0c1ddcbbf4df48
Rosanna Zhang <rosannazhang92@gmail.com>
2023-01-19 14:01:52 -0800

    Add GET req and func to generate 10 random breeds

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [breed, setBreeds] = useState({});
+
+  const getBreeds = () => {
+    return axios.get("https://dog.ceo/api/breeds/list/all")
+    .then((response) => setBreeds(response.data.message))
+  };
+
+  // generates a list of 10 random breeds *** fix: duplicates ****
+  const randomBreed = () => {
+    let arrayofBreeds = [];
+    while (arrayofBreeds.length < 10) {
+      let randomBreed = Object.keys(breed)[Math.floor(Math.random() * Object.keys(breed).length + 1)]
+      arrayofBreeds.push(randomBreed);
+      }
+      return arrayofBreeds;
+    } 
+
+
+  useEffect(() => {
+    getBreeds();
+  },[])
+
   return (
     <div className="App">
       <table className="table_animal-breed">
@@ -12,8 +36,8 @@ function App() {
         </thead>
         <tbody>
           <tr>
-            <th>1</th>
-            <th>chihuahua</th>
+          <th>1</th>
+            <th>{randomBreed()}</th>
           </tr>
           <tr>
             <th>2</th>
