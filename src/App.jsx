7e84110b5e861471aba9a6f8587983ffd99278ce
Rosanna Zhang <rosannazhang92@gmail.com>
2023-01-25 22:22:44 -0800

    Add logic to export function- still wrong format

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { initailizeTwoTables, dragAndDrop } from "./slices/breedsSlice";
+import { saveAs } from 'file-saver';
 import axios from "axios";
 import "./App.css";
 
@@ -10,52 +11,78 @@ const App = () => {
   const dispatch = useDispatch();
   const { tableOneBreeds, tableTwoBreeds, error } = useSelector(
     (state) => state.breeds
-  );
-
-  // Upon first render, get data from API & initialize two tables
-  useEffect(() => {
-    const getBreedsData = async () => {
-      const response = await axios.get(API);
-      return dispatch(initailizeTwoTables(response.data.message));
-    };
-    getBreedsData();
-  }, [dispatch]);
-
-  const handleDragStart = (e) => {
-    e.dataTransfer.setData("text/index", e.target.dataset.index);
-    e.dataTransfer.setData("text/table", e.target.dataset.table);
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const dropEndIndex = e.target.dataset.index;
-    const dropEndTable = e.target.dataset.table;
-    const dragStartIndex = e.dataTransfer.getData("text/index");
-    const dragStartTable = e.dataTransfer.getData("text/table");
-    dispatch(
-      dragAndDrop({
-        dragStartIndex,
-        dropEndIndex,
-        dragStartTable,
-        dropEndTable,
-      })
     );
-  };
-
-  // Exports the data to JSON file
-  const handleExport = (e) => {
     
-    const obj = {};
-    for (let breed of tableOneBreeds) {
-      console.log(breed);
-      for (let rank = 1; rank < tableOneBreeds.length; rank++) {
-        obj["rank" + rank] = breed;
-      }
-    }
-    console.log("obj", JSON.stringify(obj, null, 2));
-    return obj;
-    // "breed1Total" = tableOneBreeds.length
+    // Upon first render, get data from API & initialize two tables
+    useEffect(() => {
+      const getBreedsData = async () => {
+        const response = await axios.get(API);
+        return dispatch(initailizeTwoTables(response.data.message));
+      };
+      getBreedsData();
+    }, [dispatch]);
+    
+    const handleDragStart = (e) => {
+      e.dataTransfer.setData("text/index", e.target.dataset.index);
+      e.dataTransfer.setData("text/table", e.target.dataset.table);
+    };
+    
+    const handleDrop = (e) => {
+      e.preventDefault();
+      const dropEndIndex = e.target.dataset.index;
+      const dropEndTable = e.target.dataset.table;
+      const dragStartIndex = e.dataTransfer.getData("text/index");
+      const dragStartTable = e.dataTransfer.getData("text/table");
+      dispatch(
+        dragAndDrop({
+          dragStartIndex,
+          dropEndIndex,
+          dragStartTable,
+          dropEndTable,
+        })
+        );
+      };
+      
+      // Exports the data to JSON file
+      const handleExport = (e) => {
+        const dogBreeds = {};
+        const breed1Total = {};
+        const breed2Total = {};
+        const breed1Rank = {};
+        const breed2Rank = {};
+        const fileName = "dogBreeds.json";
+        
+        for (let breed of tableOneBreeds) {
+          console.log(breed);
+          for (let rank = 1; rank < tableOneBreeds.length; rank++) {
+            breed1Rank["rank" + rank] = breed;
+          }
+        }
+        
+        for (let breed of tableTwoBreeds) {
+          console.log(breed);
+          for (let rank = 1; rank < tableTwoBreeds.length; rank++) {
+            breed2Rank["rank" + rank] = breed;
+          }
+        }
+    
+     breed1Total["breed1Total"] = tableOneBreeds.length;
+     breed2Total["breed2Total"] = tableTwoBreeds.length;
 
+     const tableTwo = { ...breed2Total, breed2Rank }
+     const data = { ...breed1Total, breed1Rank, tableTwo };
+
+    dogBreeds["dogBreeds"] = data
+
+    const jsonString = JSON.stringify(dogBreeds, null, 2)
+    console.log(jsonString);
+    
+    const blob = new Blob([jsonString], { type: "application/json" })
+    saveAs(blob, fileName, err => {
+      if (err) throw error;
+    });
+
+    return blob;
   }
 
   return (
